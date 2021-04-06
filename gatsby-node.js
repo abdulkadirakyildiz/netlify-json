1899ceeaa0398e9518a9da9485712024b8f3c737
Abdülkadir <whitestardeveloper@gmail.com>
2021-04-06 23:34:41 +0300

    dd

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,20 +1,21 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
-// gatsby-node.js
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
 
   //netlify için imagenin yolunda sıkıntı çıkıyordu onu burdan müdahele ederek değiştiriyoruz...
-  if (node.relativeDirectory === `alldata`) {
-      // const imageRelativePath = node.image.replace("src/content", "..")
-      // createNodeField({
-      //   node,
-      //   name: `image`,
-      //   value: imageRelativePath,
-      // })
-     // console.log(node.image)
-  }
+  // if (node.relativeDirectory === `alldata`) {
+  //     // const imageRelativePath = node.image.replace("src/content", "..")
+  //     // createNodeField({
+  //     //   node,
+  //     //   name: `image`,
+  //     //   value: imageRelativePath,
+  //     // })
+  //    // console.log(node.image)
+  // }
+
+//  // console.log(node)
 
   if (node.internal.mediaType === `application/json`) {
     const slug = createFilePath({ node, getNode, basePath: `pages` })
