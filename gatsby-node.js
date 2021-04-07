2183520980639292e19a89040f326c1407f53d74
Abdülkadir <whitestardeveloper@gmail.com>
2021-04-07 10:15:37 +0300

    update image path

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,20 +2,7 @@ const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-
-  //netlify için imagenin yolunda sıkıntı çıkıyordu onu burdan müdahele ederek değiştiriyoruz...
-  // if (node.relativeDirectory === `alldata`) {
-  //     const imageRelativePath = node.image.replace("src/content", "..")
-  //     createNodeField({
-  //       node,
-  //       name: `image`,
-  //       value: imageRelativePath,
-  //     })
-  //     console.log(node.image)
-  // }
-
-//  // console.log(node)
+  const { createNodeField } = actions;
 
   if (node.internal.mediaType === `application/json`) {
     const slug = createFilePath({ node, getNode, basePath: `pages` })
